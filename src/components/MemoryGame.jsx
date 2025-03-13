a4b39cb8af6720d6fdab9b029218f9a992df4553
Roby Maulana <maulanar2090@gmail.com>
2025-03-13 11:01:57 +0700

    Init commit

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -1,16 +1,18 @@
+import './MemoryGame.css';
+
 const MemoryGame = ({ images }) => {
   return (
     <div>
       <h1>Memory Game</h1>
       <p>Build your memory game! </p>
       <p>Here are the sample images:</p>
-      {images.map((image) => (
-        <img
-          key={image}
-          src={image}
-          style={{ width: "300px", padding: "10px" }}
-        />
-      ))}
+      <div className="game-container">
+        {images.map((image) => (
+          <div className='game-container__img-container' key={image}>
+           <div className='game-container__img' style={{backgroundImage: `url(${image})`}}></div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
